bfcea8c7edb776bc7c6e6ddad7bdefe3d030ba1a
tejasnoovosoft <tejas.ekhande@noovosoft.com>
2025-12-11 12:42:19 +0530

    feat: update navbar site title to "Tejas.dev" and remove gradient and monospace styling

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -23,8 +23,8 @@ export default function Navbar() {
             className="fixed top-0 left-0 right-0 z-50 glass border-b-0"
         >
             <div className="container mx-auto px-4 md:px-6 h-16 flex items-center justify-between">
-                <Link href="/" className="text-xl font-bold font-mono text-gradient">
-                    &lt;Tejas /&gt;
+                <Link href="/" className="text-xl font-bold">
+                    Tejas<span className="text-primary">.dev</span>
                 </Link>
 
                 {/* Desktop Menu */}
