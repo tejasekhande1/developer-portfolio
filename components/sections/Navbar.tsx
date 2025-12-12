0b5fc2966ea5a3a5894ed388776fe13d95251eee
Tejas Ekhande <tejasekhande01@gmail.com>
2025-12-12 13:38:10 +0530

    feat: Remove Contact link from Navbar navigation items

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -11,7 +11,6 @@ const navLinks = [
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contact" },
 ];
 
 export default function Navbar() {
