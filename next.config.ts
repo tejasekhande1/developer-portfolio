e6d568112db8ec434039fa48f7db55bd2709de3c
Tejas Ekhande <tejasekhande01@gmail.com>
2025-12-12 20:32:09 +0530

    Fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: "export",
     images: {
         unoptimized: true,
     },
-    basePath: '/your-repo-name',
-    assetPrefix: '/your-repo-name/',
 };
 
 module.exports = nextConfig;
