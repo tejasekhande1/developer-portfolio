a55e27aeaf4a2b4a4fb81c21174baa6cf5e41e0a
Tejas Ekhande <tejasekhande01@gmail.com>
2025-12-12 20:37:09 +0530

    Revert "Fix next.config.ts"
    This reverts commit e6d568112db8ec434039fa48f7db55bd2709de3c.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    output: 'export',
     images: {
         unoptimized: true,
     },
+    basePath: '/your-repo-name',
+    assetPrefix: '/your-repo-name/',
 };
 
 module.exports = nextConfig;
